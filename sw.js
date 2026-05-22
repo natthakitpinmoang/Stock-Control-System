de88e3f041a92a886f2857ec94644c02ccfb9811
natthakitpinmoang <72189543+natthakitpinmoang@users.noreply.github.com>
2026-05-22 14:36:14 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "inventory-system-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      // ถ้ามีข้อมูลใน Cache ให้ดึงมาใช้เลย (ทำให้เปิดแอปแบบ Offline ได้)
+      return response || fetch(e.request);
+    })
+  );
+});
